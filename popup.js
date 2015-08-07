aa37d9a6b7631d2f84594df4294513ee5a9fc242
Kyle Maurer <kyle@realbigmarketing.com>
2015-08-07 12:09:14 -0400

    Got the javascript actually working. Woohoo! Now we're posting to Slack!

--- popup.js
+++ popup.js
@@ -3,24 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     postToSlackButton.addEventListener('click', function() {
 
         chrome.tabs.getSelected(null, function(tab) {
-            d = document;
-
-            var post = new Object();
-            // This gets the URL of the current tab
-            post.text = tab.url;
-            post.username = 'Pick of the Week Idea';
-            var filter = new Array();
-            filter[0] = "text";
-            filter[1] = "username";
-            var f = d.createElement('form');
-            f.action = 'https://hooks.slack.com/services/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';
-            f.method = 'post';
-            var i = d.createElement('input');
-            i.type = 'hidden';
-            i.name = 'url';
-            i.value = JSON.stringify(post, filter);
-            f.appendChild(i);
-            d.body.appendChild(f);f.submit();
+            xhr = new XMLHttpRequest();
+            xhr.open("POST", "https://wprt.slack.com/services/hooks/slackbot?token=dFFotg4Aim0Hbbke0V8No9ti&channel=%23general", true);
+            xhr.send("Pick of the week idea: " + tab.title + " " + tab.url);
         });
     }, false);
 }, false);
